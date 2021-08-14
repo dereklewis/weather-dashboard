f830852357b646d8d41a82ef071e2155f239f108
Derek Lewis <derek.lewis.den@outlook.com>
2021-08-14 12:15:52 -0600

    linked to API and added text

---
+++ assets/JS/script.js
@@ -0,0 +1,32 @@
+var apiKey = "eba4077c45d00ac6014f061dfc8fe378";
+
+
+ 
+
+
+var submitCity = document.querySelector("#submit-city");
+var city = " ";
+
+$("#submit-button").click(function(){
+
+var citySelection = $("#city-name").val()
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + citySelection + "&appid=" + apiKey + "&units=imperial";
+  $.ajax({
+    url: queryURL, success: function(result){
+    console.log(result);
+    
+    $(".city-info").text(result.name);
+    // $(".current-date").text(result.dt);
+    var curDate = $(result.dt);
+    console.log(curDate);
+
+    $("#current-temp").text(result.main.temp);
+    $("#humidity").text(result.main.humidity);
+    $("#wind-speed").text(result.wind.speed);
+    
+  }});
+});
+
+
+
+
